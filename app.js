155cacdfc8426219ec4c6ec0de0282a12a9065ac
Shubham Dangi <shubhamdangi@Shubhams-MacBook-Air.local>
2022-04-07 00:45:38 +0530

    task18

--- app.js
+++ app.js
@@ -7,10 +7,22 @@ const errorController = require('./controllers/error');
 
 const sequelize = require('./util/database');
 
+const Product = require('./models/product');
+const User = require('./models/user');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
+app.use((req, res, next) => {
+    User.findByPk(1)
+      .then(user => {
+        req.user = user;
+        next();
+      })
+      .catch(err => console.log(err));
+  });
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -23,8 +35,27 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync().then(result => {
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
+sequelize
+  //.sync({ force: true })
+  .sync()
+  .then(result => {
+    return User.findByPk(1);
+    // console.log(result);
+  })
+  .then(user => {
+    if (!user) {
+      return User.create({ name: 'Max', email: 'test@test.com' });
+    }
+    return user;
+  })
+  .then(user => {
+    // console.log(user);
     app.listen(3000);
-    })
-    .catch((err)=> console.log(err));
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
--- controllers/admin.js
+++ controllers/admin.js
@@ -16,14 +16,21 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
-    title: title,
-    price: price,
-    imageUrl: imageUrl,
-    description: description
-  })
-  .then(result => console.log(result))
-  .catch(err => console.log(err))
+  req.user
+    .createProduct({
+      title: title,
+      price: price,
+      imageUrl: imageUrl,
+      description: description
+    })
+    .then(result => {
+      // console.log(result);
+      console.log('Created Product');
+      res.redirect('/admin/products');
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 exports.getEditProduct = (req, res, next) => {

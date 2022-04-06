836bed6b1be85a94398a7a454eea3aa5645928d6
Shubham Dangi <shubhamdangi@Shubhams-MacBook-Air.local>
2022-04-07 01:53:41 +0530

    last task

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const sequelize = require('./util/database');
 
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -37,6 +39,11 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+
 
 sequelize
   //.sync({ force: true })
@@ -53,6 +60,9 @@ sequelize
   })
   .then(user => {
     // console.log(user);
+    return user.createCart();
+  })
+  .then(cart => {
     app.listen(3000);
   })
   .catch(err => {
